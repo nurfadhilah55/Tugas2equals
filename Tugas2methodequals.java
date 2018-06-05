5394251e099d2906e998eadfa4b30229f445fac9
nurfadhilah55 <39688096+nurfadhilah55@users.noreply.github.com>
2018-06-05 16:36:05 +0700

    Add files via upload

---
+++ Tugas2methodequals.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication8;
+
+/**
+ *
+ * @author ACER
+ */
+public class Tugas2methodequals {
+     public static void main(String[] args) {
+        String st1="saya suka makan bakso";
+        String st2 ="saya suka makan bakso";
+        String st3 ="saya ";
+        String st4 ="suka makan ";
+        String st5 ="bakso";
+        String st6 =st3+st4+st5;
+        System.out.println("st 1 : "+ st1);
+        System.out.println("st 2 : "+ st2);
+        System.out.println("st 3 : "+ st3);
+        System.out.println("st 4 : "+ st4);
+        System.out.println("st 5 : "+ st5);
+        System.out.println("st 6 : "+ st6);
+        System.out.println("Membandingan dengan Operator ==");
+        System.out.println("st 1 == st 2 : "+ (st1==st2));
+        System.out.println("st 6 == st 1 : "+ (st6==st1));
+        System.out.println("st 6 == st 2 : "+ (st6==st2));
+        System.out.println("Membandingan dengan Metode equal");
+        System.out.println("statement1.equal(st2): "+(st1.equals(st2)));
+        System.out.println("statement6.equal(st1): "+(st6.equals(st1)));
+        System.out.println("statement6.equal(st2): "+(st6.equals(st2)));
+        System.out.println("jadi kesimpulannya perbandingan menggunakan operator == hanya true pada statement 1 banding statement 2 selain itu false");
+        System.out.println("sementara pada metode equel semuanya true");
+    }
+
+} 
+   
